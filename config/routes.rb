e81d2a6efa804effb7c4aeb8c9a5a6cf60ce9d94
Katie Lonsdale <katie952@gmail.com>
2023-04-18 15:39:50 -0600

    refactor: use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "/dishes/:id", to: "dishes#show"
+  resources :dishes, only: [:show]
+  resources :chefs, only: [:show]
+  resources :ingredient_dishes, only: [:create]
 
   get "chefs/:id/ingredients", to: "chefs/ingredients#index"
-
-  get "chefs/:id", to: "chefs#show"
-
-  post "/ingredient_dishes", to: "ingredient_dishes#create"
 end
