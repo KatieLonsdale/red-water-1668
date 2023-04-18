7a96906987205996ed4af5b7d1f9c5364f353fe5
Katie Lonsdale <katie952@gmail.com>
2023-04-18 13:48:57 -0600

    test: add tests for calories and chef_name methods

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Dish, type: :model do
     it {should have_many(:ingredient_dishes)}
     it {should have_many(:ingredients).through(:ingredient_dishes)}
   end
+
+  describe "instance methods" do
+    describe "#calories" do
+      it "returns the calories for a dish" do
+        test_data
+        expect(@dish_1.calories).to eq(320)
+        expect(@dish_2.calories).to eq(870)
+      end
+    end
+    describe "#chef_name" do
+      it "returns the name of the chef the dish belongs to" do
+        test_data
+        expect(@dish_1.chef_name).to eq(@chef_1.name)
+        expect(@dish_3.chef_name).to eq(@chef_2.name)
+      end
+    end
+  end
 end
