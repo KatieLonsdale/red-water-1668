f29fc134a5ab8b759a9ef2e8621c590d54979fc2
Katie Lonsdale <katie952@gmail.com>
2023-04-18 15:30:37 -0600

    test: add test for displaying three most popular ingredients

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe "Chef show page" do
       click_link("What ingredients am I using?")
       expect(current_path).to eq("/chefs/#{@chef_1.id}/ingredients")
     end
+    it "shows me the three most popular ingredients that the chef uses" do
+      visit "/chefs/#{@chef_1.id}"
+      expect(page).to have_content(@ingredient_1.name)
+      expect(page).to have_content(@ingredient_2.name)
+      expect(page).to have_content(@ingredient_3.name)
+      expect(page).to have_no_content(@ingredient_4.name)
+      expect(page).to have_no_content(@ingredient_5.name)
+      expect(page).to have_no_content(@ingredient_6.name)
+      expect(page).to have_no_content(@ingredient_7.name)
+
+      expect(@ingredient_1.name).to appear_before(@ingredient_2.name)
+      expect(@ingredient_2.name).to appear_before(@ingredient_3.name)
+    end
   end
 end
