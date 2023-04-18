0e7692cf1049072626781d1b943dbdb57e3af950
Katie Lonsdale <katie952@gmail.com>
2023-04-18 14:14:32 -0600

    test: add tests for functionality to add ingredient to a dish

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -29,5 +29,27 @@ RSpec.describe "Dish show page" do
       expect(page).to have_content("Chef: #{@chef_2.name}")
       expect(page).to have_no_content(@chef_1.name)
     end
+    it "has a form to add an existing ingredient to that dish" do
+      visit "/dishes/#{@dish_1.id}"
+      expect(page).to have_content("Add Ingredients by ID")
+        fill_in :ingredient_id, with: "#{@ingredient_4.id}"
+        expect(page).to have_selector(:link_or_button, 'Add')
+        click_button "Add"
+    end
+    it "redirects me to the dish show page after submitting the form" do
+      visit "/dishes/#{@dish_1.id}"
+      fill_in :ingredient_id, with: "#{@ingredient_4.id}"
+      click_button "Add"
+      
+      expect(current_path).to eq("/dishes/#{@dish_1.id}")
+    end
+    it "displays the new ingredient after submitting the form" do
+      visit "/dishes/#{@dish_1.id}"
+      expect(page).to have_no_content(@ingredient_4.name)
+      fill_in :ingredient_id, with: "#{@ingredient_4.id}"
+      click_button "Add"
+
+      expect(page).to have_content(@ingredient_4.name)
+    end
   end
   
